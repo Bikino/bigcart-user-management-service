1f9012dd984b805d7d792854e9e9f85b92d85d73
Ildephonse BIKINO <ildephonsebikino@Ildephonses-MacBook-Pro.local>
2020-10-19 09:15:25 -0400

    First commit

---
+++ src/main/java/com/bigcart/user/managementservice/bigcartusermanagementservice/BigcartUserManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bigcart.user.managementservice.bigcartusermanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigcartUserManagementServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BigcartUserManagementServiceApplication.class, args);
+    }
+
+}
