ca8a9b7a907b1e0291ee3324f3b8201cdf4c93d2
Mohamed AbdElZaher <hamada6_4@hotmail.com>
2020-10-23 19:25:56 -0500

    Address Entity.

--- src/main/java/com/bigcart/user/managementservice/bigcartusermanagementservice/domain/repository/PersonRepository.java
+++ src/main/java/com/bigcart/user/managementservice/bigcartusermanagementservice/domain/repository/PersonRepository.java
@@ -1,8 +1,7 @@
 package com.bigcart.user.managementservice.bigcartusermanagementservice.domain.repository;
 
+import com.bigcart.user.managementservice.bigcartusermanagementservice.domain.model.Person;
 import org.springframework.data.repository.CrudRepository;
-
-import com.bigcart.user.managementservice.bigcartusermanagementservice.domain.model.User;
 
 
 
@@ -11,6 +10,6 @@ import com.bigcart.user.managementservice.bigcartusermanagementservice.domain.mo
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 
-public interface UserRepository extends CrudRepository<User, Integer> {
+public interface PersonRepository extends CrudRepository<Person, Integer> {
 
 }
